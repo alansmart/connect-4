1f24c4df6499ed9249d23705268ed5833ef3db6a
Alankar <sharma.alankar831@gmail.com>
2020-05-12 18:18:22 +0530

    Add files via upload
    updated the files to follow HTML, CSS and JavaScript procedure instead of canvas approach.

--- script.js
+++ script.js
@@ -1,119 +1,146 @@
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
 
-var paddleWidth = 200;
-var paddleHeight = 150;
-var paddleX = (canvas.width-paddleWidth)/2;
-var circleRadius= 10;
-var circleRows = 6;
-var circleColumns = 7;
-var ballRadius = 10;
-var x = canvas.width / 2;
-var y = ballRadius;
-var dx = 2;
-var dy = 2;
-var rightPressed = false;
-var leftPressed = false;
-var circlePadding = 5;
-var circleOffsetTop = 30;
-var circleOffsetLeft = 30;
+var game_active = false; // This will be used to prevent being able to drop pieces once the game is over
+var active_player = 0; // the # of the active player - 1 or 2.  Default is 0, meaning no active player
+var gameboard = []; // define the gameboard as an array.  We will later set it up as a multi-dimensional array, to represent the col/row value for the game board
+var player_color = []; //define player_color as an array
+player_color[1] = "red"; //set the player_color for player 1 to "red" 
+player_color[2] = "blue"; // set the player_color for player 2 to "blue"
 
-// Key Eveent Handlers
-document.addEventListener("keydown", keyDownHandler);
-document.addEventListener("keyup", keyUpHandler);
+function beginGame() {
+  //don't reset the game until the last one is done.  So if the game is still active, return (don't continue with
+  //the function)  When you return, the rest of the function is skipped, and the value you return will be
+  //passed to the function that called the function.
+  if (game_active == true) return false; 
 
-function keyDownHandler(e) {
-    if(e.keyCode == 37) {
-        rightPressed = true;
-    } else if(e.keyCode == 39) {
-        leftPressed = true;
-    }
+  game_active = true;  //We're starting the game, so make game_active = true
+  //loop for creation of gameBoard
+  for (row=0; row<=5; row++) {
+    gameboard[row] = [];
+    for (col=0; col<=6; col++) {
+      gameboard[row][col] = 0;
+    }	
+  }		
+  
+  drawBoard(); // call the function to draw the board.				
+  active_player = 1; //set the first player as their turn
+  setUpTurn(); //get ready for the player's turn
 }
 
-function keyUpHandler(e) {
-    if(e.keyCode == 37) {
-        rightPressed = false;
-    } else if(e.keyCode == 39) {
-        leftPressed = false;
-    }
+/* drawBoard will draw the board - it will update each item to make sure it is the appropriate value */
+function drawBoard() {
+  checkForWin(); //check to see if any player has won.
+  for (col = 0; col<=6; col++) {
+    for (row=0; row<=5; row++) {
+      //Set the inner HTML of the square (a td) to be a span with the class of 'piece' and 'player' + the value of that 
+      //gameboard piece.  Using CSS, you can style player0, player1 and player2 so that the square will appear correctly.
+      document.getElementById('square_'+row+'_'+col).innerHTML ="<span class='piece player"+gameboard[row][col]+"'> </span>";
+    }	
+  }
 }
 
-var circlesArr = [];
-function initTheCricles() {
-    for(c=0; c< circleColumns; c++) {
-        circlesArr[c] = [];
-        for(r=0; r < circleRows; r++) {
-            circlesArr[c][r] = {x:0, y:0, status:1}
+function checkForWin() {
+  /* Ultimately there are 4 ways to win - left-to-right, diagnol up, diagnol down, and top to bottom.  This 
+  function will check each of these situations twice - one for each player. */
+  
+  //check left-to-right
+  //check for player 1 and 2
+  for (i=1; i<=2; i++) {
+    //since a winning row must be 4 long, we only need to check the first 3 rows, 0,1,and 2
+    for (col = 0; col <=3; col++) {
+      for (row = 0; row <=5; row++) {
+        //check to see if the gameboard item is set to the player we are checking, if so, lets check the next 3 for a match
+        if (gameboard[row][col] == i) {
+          if ((gameboard[row][col+1] == i) && (gameboard[row][col+2] == i) && (gameboard[row][col+3] == i)) {
+            endGame(i);//a match has been made, so run EndGame with the player that had the win
+            return true; //stop checking for a win - the game is over.
+          }
         }
+      }
     }
-}
-
-function drawCircles() {
-    for(c=0; c < circleColumns; c++) {
-        for(r=0; r < circleRows; r++) {
-            if(circlesArr.status == 1) {
-                var circleX = (c*(circleRadius+circlePadding)) + circleOffsetLeft;
-                var circleY = (r*(circleRadius+circlePadding)) + circleOffsetTop;
-                circles[c][r].x = circleX;
-                circles[c][r].y = circleY;
-                ctx.beginPath();
-                ctx.arc(circleX, circleY, circleRadius, Math.PI);
-                ctx.fillStyle = '#ffff';
-                ctx.fill();
-                ctx.closePath();
-            }
+  }
+  
+  //check top-to-bottom
+  for (i=1; i<=2; i++) {
+    //since a winning row must be 4 long, we only need to check the first 3 rows, 0,1,and 2
+    for (col = 0; col <=6; col++) {
+      for (row = 0; row <=2; row++) {
+        //check to see if the gameboard item is set to the player we are checking, if so, lets check the next 3 for a match
+        if (gameboard[row][col] == i) {
+          if ((gameboard[row+1][col] == i) && (gameboard[row+2][col] == i) && (gameboard[row+3][col] == i)) {
+            endGame(i); //a match has been made - run endGame for the player who had the match.
+            return true; //stop checking for a win - the game is over.
+          }
         }
+      }
+    }
+  }
+  
+  //check diagnol down
+  for (i=1; i<=2; i++) {
+    //since a winning row must be 4 long, we only need to check the first 3 rows, 0,1,and 2
+    for (col = 0; col <=3; col++) {
+      //we also only need to check the bottom most columns - as the win must be upwards
+      for (row = 0; row <=2; row++) {
+        //check to see if the gameboard item is set to the player we are checking, if so, lets check the next 3 for a match
+        if (gameboard[row][col] == i) {
+          if ((gameboard[row+1][col+1] == i) && (gameboard[row+2][col+2] == i) && (gameboard[row+3][col+3] == i)) {
+            endGame(i);
+            return true;
+          }
+        }
+      }
+    }
+  }
+          
+  //check diagnol up
+  for (i=1; i<=2; i++) {
+    //since a winning row must be 4 long, we only need to check the first 3 rows, 0,1,and 2
+    for (col = 0; col <=3; col++) {
+      //we also only need to check the bottom most columns - as the win must be upwards
+      for (row = 3; row <=5; row++) {
+        //check to see if the gameboard item is set to the player we are checking, if so, lets check the next 3 for a match
+        if (gameboard[row][col] == i) {
+          if ((gameboard[row-1][col+1] == i) && (gameboard[row-2][col+2] == i) && (gameboard[row-3][col+3] == i)) {
+            endGame(i);
+            return true;
+          }
+        }
+      }
+    }
+  }
+}
+
+/* endGame will end the game - any additional functions or things you want to happen when the game is over can go here */
+function endGame(winningPlayer) {
+  game_active = false; //set the "game_active" to false, so that it can be started again.
+  document.getElementById('game_info').innerHTML = "Winner: " + winningPlayer; //set the "game_info" to the winner and the winning player #
+}
+
+/* setUpTurn will display who is the active player */
+function setUpTurn() {
+  if (game_active) { //only run this is the game is active.
+    //display the current player, and create a <span> with the class of the player# so that it will show the color.
+    document.getElementById('game_info').innerHTML = "Current Player: Player " + active_player + " <span class='player"+active_player+"'>(" + player_color[active_player] + ")</span>";
+  }
+}			
+
+/* drop will add a piece to the lowest available column */
+function drop(col) {
+    /* Look for the lowest point in this row that is open */
+    //the opposite of our loop above - as we're going to start from the bottom looking for an open slot
+    for (row=5; row>=0; row--) { //note: we are using row--, which will reduce the value of row by 1, the opposite of ++
+      if (gameboard[row][col] == 0) {
+        //set the empty row to the active player's number
+        gameboard[row][col] = active_player;
+        drawBoard(); // draw the board.
+        //change the active players turn: 
+        active_player == 1 ? active_player = 2 : active_player = 1;
+        //there is also a fancy way of doing this call a ternary assignment that looks like this: active_player = (active_player == 1) ? 2 : 1;
+        
+        setUpTurn(); //display who is the active player
+
+        //stop looking for empty spaces
+        return true;
+      }
     }
 }
-
-
-function collisionDetector() {
-
-}
-
-//Draw a Net
-function drawCollector() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-   // drawCircles();
-    ctx.arc(paddleWidth+3, paddleHeight+3, 0, 0, Math.PI*2);
-    ctx.fillStyle = '#000';
-    ctx.fill();
-    ctx.closePath();
-}
-
-//Draw ball
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = '#000DFF';
-    ctx.fill();
-    ctx.closePath();
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawCollector();    
-    y += dy;
-
-    if(rightPressed && paddleX < canvas.width-paddleWidth) {
-		paddleX += 7;
-	}
-	else if(leftPressed && paddleX > 0) {
-		paddleX -= 7;
-	}
-
-    requestAnimationFrame(draw);
-}
-
-document.addEventListener("mousemove", mouseMoveHandler);
-
-function mouseMoveHandler(e) {
-	var relativeX = e.clientX - canvas.offsetLeft;
-	if(relativeX > 0+paddleWidth/2 && relativeX < canvas.width-paddleWidth/2) {
-		paddleX = relativeX - paddleWidth/2;
-	}
-}
-
-draw();
